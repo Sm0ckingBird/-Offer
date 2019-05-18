bef72947f469bf461f4f8251a63d5b3303814e92
Sparrow <1158443037@qq.com>
2019-05-18 23:22:13 +0800

    May 18 Num 37

---
+++ src/剑指offer/Num37_FirstCommonNode.java
@@ -0,0 +1,21 @@
+package 剑指offer;
+import util.ListNode;
+
+public class Num37_FirstCommonNode {
+	
+	public static ListNode FirstCommonNode(ListNode l1, ListNode l2) {
+		
+		//先在长链表上把两个链表的长度差走完,再同时遍历
+		
+		return null;
+		
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ListNode l1 = new ListNode(1);
+		ListNode l2 = new ListNode(2);
+	}
+
+}
