640ca76ca413b213c57baceb0d63c9f50787aaa6
Sparrow <1158443037@qq.com>
2019-05-21 10:56:26 +0800

    May 21 Num 46

---
+++ src/forty/Num46_SumOfN.java
@@ -0,0 +1,21 @@
+package forty;
+
+public class Num46_SumOfN {
+	
+	//C++
+	
+	//构造函数——循环
+	
+	//虚函数——递归
+	
+	//函数指针求解
+	
+	//利用模版类型
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
