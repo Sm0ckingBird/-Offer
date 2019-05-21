e37c7ad299c65dee6371a3490a99f4c1bc3ee0b3
Sparrow <1158443037@qq.com>
2019-05-21 11:59:34 +0800

    May 21 Num 50

---
+++ src/forty/Num50_PublicNode.java
@@ -0,0 +1,14 @@
+package forty;
+
+public class Num50_PublicNode {
+	
+	//使用辅助空间，将问题转换为链表的公共节点
+	
+	//直接遍历，但会出现很多重复
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
