4f1b8e42844043dd99a311e2f2080b9f2e70ab1d
Sparrow <1158443037@qq.com>
2019-05-21 18:11:45 +0800

    May 21 Num 3

--- src/start/Num3_SearchIn2DArray.java
+++ src/start/Num3_SearchIn2DArray.java
@@ -4,6 +4,29 @@ public class Num3_SearchIn2DArray {
 	
 	//从右上角或者坐下角开始搜寻
 	//pass
+	
+	public static boolean Find(int[][] matrix, int rows, int columns, int number) {
+		
+		if(matrix == null || rows < 1 || columns < 1)
+			return false;
+		
+		int r = 0;
+		int col = columns - 1;
+		int cur = 0;
+		
+		
+		while(true) {
+			cur = matrix[r][col];
+			if(cur == number)
+				return true;
+			else if(cur > number)
+				col--;
+			else
+				r++;
+			if(r == rows || col == -1)
+				return false;
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
