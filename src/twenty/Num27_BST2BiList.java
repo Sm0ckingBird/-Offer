8b6e695f9cb5f806c18606ba2b16c84286aece17
Sparrow <1158443037@qq.com>
2019-05-25 10:05:09 +0800

    May 25 Finished

--- src/twenty/Num27_BST2BiList.java
+++ src/twenty/Num27_BST2BiList.java
@@ -1,38 +1,53 @@
 package twenty;
 
 
-import java.util.Stack;
-
 import util.BinaryTreeNode;
 
 public class Num27_BST2BiList {
 	
 	public static BinaryTreeNode BST2BiList(BinaryTreeNode root) {
-		if(root == null)
+		BinaryTreeNode last = ConvertNode(root);
+		while(last.left != null)
+			last = last.left;
+		return last;
+	}
+	
+	public static BinaryTreeNode ConvertNode(BinaryTreeNode node) {
+		if(node == null)
 			return null;
 		
-		Stack<BinaryTreeNode> s = new Stack<BinaryTreeNode>();
-		BinaryTreeNode cur = root;
+		BinaryTreeNode cur = node;
+		BinaryTreeNode last = null;
 		
-		while(cur != null || !s.isEmpty()) {
-			while(cur != null) {
-				s.add(cur);
-				cur = cur.left;
-			}
-			if(s.isEmpty()) {
-				cur = s.pop();
-			}
-		}
+		if(node.left != null)
+			 last = ConvertNode(cur.left);
 		
+		cur.left = last;
 		
-		return root;
+		if(last != null)
+			last.right = cur;
+		
+		if(cur.right != null)
+			ConvertNode(cur.right);
+		
+		return cur;
 	}
 	
 	
-	
-	
 	public static void main(String[] args) {
+		BinaryTreeNode a = new BinaryTreeNode(10);
+		BinaryTreeNode b = new BinaryTreeNode(6);
+		BinaryTreeNode c = new BinaryTreeNode(14);
+		BinaryTreeNode d = new BinaryTreeNode(4);
+		BinaryTreeNode e = new BinaryTreeNode(8);
+		BinaryTreeNode f = new BinaryTreeNode(12);
+		BinaryTreeNode g = new BinaryTreeNode(16);
 		
+		a.left = b; a.right = c;
+		b.left = d; b.right = e;
+		c.left = f; c.right = g;
+		
+		BST2BiList(a);
 	}
 
 }
